14446fb753271e671170c1d70895603ea03ebcdf
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-09-28 14:44:48 +0900

    一旦コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # デフォルトであったやつ↓
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # リダイレクト処理を用意しましょう-あってるかな？↓
+  before_action :move_to_index, except: [:index, :show]
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+# ここも入れてみた↓
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+
 end
