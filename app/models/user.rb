403e7e94f27cec239bbc6c4a1e5e762214eeb779
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-09-29 19:26:45 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+validates :birth_day, presence: true
+
+validates :nickname, presence: true
+# validates :password, presence: true
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+
+
+#バリデーションを加えたところ↓
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates :last_name_zenkaku_kanji
+  validates :first_name_zenkaku_kanji
+  validates :first_name_kana
+  validates :last_name_kana
+  
+  
+  
+
+
+        end
+      end
