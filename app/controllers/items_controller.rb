108e62e1177240b2d6181159f00ea73ee7b8f437
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-04 09:37:03 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,14 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show, :search]
   def index
     @items = Item.all
+    
+  end
+  
+
+  # 画像の保存を許可するストロングパラメーター?user_idをitems_id?
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
