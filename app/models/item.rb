0f5c0127a524cae2d7113d414a34cc0b0fdaf32e
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-05 17:11:14 +0900

    collection_select実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :category 
+  belongs_to_active_hash :status
+  belongs_to_active_hash :place_of_origin
+  belongs_to_active_hash :delivery_term
+  belongs_to_active_hash :shipping_status
+
    #空の投稿を保存できないようにする
-   validates :title, :text, :genre, presence: true
+   validates :name, :price, :user, :title, :text, :genre, presence: true
 
    #ジャンルの選択が「--」の時は保存できないようにする
    validates :genre_id, numericality: { other_than: 1 } 
+   validates :category_id, numericality: { other_than: 1 } 
+   validates :status_id, numericality: { other_than: 1 } 
+   validates :delivery_terms_id, numericality: { other_than: 1 } 
+   validates :place_of_origin_id, numericality: { other_than: 1 } 
+   validates :shipping_status_id, numericality: { other_than: 1 } 
+
 
   has_one_attached :image
   belongs_to:user
