c02eaa1a616fc36b98ce080615701d222aba6d9c
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-10 18:07:17 +0900

    コントローラーの更新update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,6 +23,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    
+  end
+
+  def update
     
   end
 
