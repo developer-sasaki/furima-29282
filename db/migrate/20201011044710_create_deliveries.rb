e61307790e80d6081f494e0ba09a6f834c876287
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-11 14:06:16 +0900

    purchaseとdelivery dbマイグレーション実行

--- db/migrate/20201011044710_create_deliveries.rb
+++ db/migrate/20201011044710_create_deliveries.rb
@@ -1,6 +1,12 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
+      t.string :post_code           , null: false
+      t.integer :city               , null: false
+      t.string :street1             , null: false
+      t.string :building            , null: false
+      t.string :phone               , null: false
+      t.references :item_purchase   , null: false,foreign_key: true
 
       t.timestamps
     end
