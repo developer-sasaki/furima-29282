d67befb93ae644d26b58642a34fcdc36c5ec3f6c
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-11 19:41:40 +0900

    item_purchasesコントローラー実装

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,17 +1,22 @@
 class ItemPurchasesController < ApplicationController
   def index
+    @item_purchases = ItemPurchase.new
   end
 
-    # def create
-    #   item_purchases.create(item_purchases_params)
-    #   redirect_to root_path
-    # end
-  
-    # private
-    # def item_purchases_params
-    #   params.require(:item_purchases).permit(:user_id,item_id:).merge(user_id: current_user.id, items_id: params[:item_id])
-    # end
-  
+  def create
+    @item_purchases = ItemPurchase.new(item_purchases_params)
+    if @order.valid?
+      @item_purchases.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
 
-    # form object実装後修正
+  private
+
+  def item_purchases_params
+    params.require(:item_purchases).permit(:price)
+  end
+
 end
