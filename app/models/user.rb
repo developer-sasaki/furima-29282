b1d8655f2edd81f7bb393ffd6fb96fab9fd53bfc
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-11 15:54:58 +0900

    モデルアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :item
+  has_many :item_purchase
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
