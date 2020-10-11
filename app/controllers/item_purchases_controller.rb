6aae261185ccb22cb5df863820798b90dcb09044
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-11 18:01:25 +0900

    routesの変更showの変更など

---
+++ app/controllers/item_purchases_controller.rb
@@ -0,0 +1,17 @@
+class ItemPurchasesController < ApplicationController
+  def index
+  end
+
+    # def create
+    #   item_purchases.create(item_purchases_params)
+    #   redirect_to root_path
+    # end
+  
+    # private
+    # def item_purchases_params
+    #   params.require(:item_purchases).permit(:user_id,item_id:).merge(user_id: current_user.id, items_id: params[:item_id])
+    # end
+  
+
+    # form object実装後修正
+end
