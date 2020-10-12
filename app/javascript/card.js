a43fe1c5b54cd9d34350280d33ec6e8ff3e41cf3
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-12 12:13:13 +0900

    item_purchase vieの実装とjavascript card実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,5 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
