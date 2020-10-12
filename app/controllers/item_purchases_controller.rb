865857ea330ff9d290120e45d4667cf3d11cc1ce
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-12 21:23:20 +0900

    payjp実装

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,13 +1,16 @@
 class ItemPurchasesController < ApplicationController
   def index
     @item_purchases = ItemPurchase.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @item_purchases = ItemPurchase.new(item_purchase_params)
     if @item_purchases.valid?
+      pay_item
       @item_purchases.save
       return redirect_to root_path
+    
     else
       render 'index'
     end
@@ -16,7 +19,16 @@ class ItemPurchasesController < ApplicationController
   private
 
   def item_purchase_params
-    params.require(:item_purchase).permit(:price).merge(token: params[:token])
+    params.permit(:user, :item, :postal_code, :place_of_origin, :city, :addresses, :building, :phone_number,:item_id).merge(token: params[:token])
   end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+    amount: item_purchase_params[:price],  # 商品の値段
+    card: item_purchase_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end
 
 end
--- spec/models/item_purchase_spec.rb
+++ spec/models/item_purchase_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe ItemPurchase, type: :model do
+RSpec.describe Order, type: :model do
   before do
-    @item_purchases = FactoryBot.build(:item_purchase)
+    @order = FactoryBot.build(:order)
   end
 
-  it "priceがあれば保存ができること" do
-    expect(@item_purchases).to be_valid
+  it "priceとtokenがあれば保存ができること" do
+    expect(@order).to be_valid
   end
 
-  it "priceが空では保存ができないこと" do
-    @item_purchases.price = nil
-    @item_purchases.valid?
-    expect(@item_purchases.errors.full_messages).to include("Price can't be blank")
+  it "priceが空では登録できないこと" do
+    @order.price = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it "tokenが空では登録できないこと" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
   end
 end
