fa3ed1113383463eba1721ee988ba82729054d3b
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-13 10:42:25 +0900

    フォームオブジェクト構築中の修正

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,9 +1,12 @@
 class ItemPurchase < ApplicationRecord
-  attr_accessor :token
+ attr_accessor :token,:
+  
   validates :price, presence: true
   validates :token, presence: true
 
   belongs_to :user
   belongs_to :item
   has_one :delivery
+
+  
 end
