14c4df194ad122bae625aa75233ea008dd25e73b
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-15 11:24:08 +0900

    validation調整

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -2,9 +2,9 @@ class FormObject
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :street1, :building, :phone, :token
 
-  validates :user_id, :item_id, :city, :street1, :building, :token, presence: true
 
   with_options presence: true do
+    validates :user_id, :item_id, :city, :street1, :building, :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Post code can't be blank" }
     validates :prefecture_id, numericality: { other_than: 1, message: 'prefecture must be other than 1' }
     validates :phone, numericality: { with: /\A\d{11}\z/, message: " phone can't be blank" }
