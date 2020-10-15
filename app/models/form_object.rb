d76f06f2030b9fbe824494c335e275d5a61ae267
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-10-15 15:39:16 +0900

    バリデーションからビルディング削除

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -4,7 +4,7 @@ class FormObject
 
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :street1, :building, :token
+    validates :user_id, :item_id, :city, :street1, :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Post code can't be blank" }
     validates :prefecture_id, numericality: { other_than: 1, message: 'prefecture must be other than 1' }
     validates :phone, numericality: { with: /\A\d{11}\z/, message: " phone can't be blank" }
